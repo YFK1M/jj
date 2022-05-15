b3926642dac55929362a61e6e1beb55e0b3ca5ec
Yfk1m <laystimer@gmail.com>
2022-05-15 19:01:48 +0300

    fdf

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(8000);
 }
 
